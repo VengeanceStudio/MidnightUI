fef9efb2ef2cc068ffa76d9b7aac11772a628449
Chorthee <chriskinzey70@gmail.com>
2026-02-06 12:12:03 -0500

    Reduce widget frame x-offset by 5 pixels
    In SkinAceGUIWidget (Core.lua), decrease the horizontal offset applied when repositioning widget frames from +35 to +30. This adjusts widget alignment/spacing by shifting frames 5 pixels left to improve layout.

--- Core.lua
+++ Core.lua
@@ -726,7 +726,7 @@ function MidnightUI:SkinAceGUIWidget(widget, widgetType)
             local point, relativeTo, relativePoint, xOfs, yOfs = widget.frame:GetPoint()
             if point and xOfs and yOfs then
                 widget.frame:ClearAllPoints()
-                widget.frame:SetPoint(point, relativeTo, relativePoint, xOfs + 35, yOfs)
+                widget.frame:SetPoint(point, relativeTo, relativePoint, xOfs + 30, yOfs)
             end
         end
         
